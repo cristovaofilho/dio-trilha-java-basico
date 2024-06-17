a3bd33c154c634ad166147b25237ddef2c14cf4c
cristovao <academic.cristovao@gmail.com>
2024-06-17 15:59:44 -0400

    Operadores - 7

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,28 +1,19 @@
 public class Operadores {
     public static void main(String[] args) {
         
+        boolean condicao1=true;
 
-        String nomeUm = "Cristóvão";
-        String nomeDois = new String("Cristóvão");
+        boolean condicao2=false;
 
-        System.out.println(nomeUm.equals(nomeDois));
-
-        int numero1 = 1;
-        int numero2 = 2;
-
-        boolean simNao = numero1 == numero2;
-
-        if(numero1 == numero2){
-            System.out.println("a nossa condição é verdadeira");
+        if (condicao1 && (7 > 4)) {
+            System.out.println("as duas condições são verdadeiras");
         }
 
-        System.out.println("numero1 é igual a numero2? " + "numero1 " + numero1 + " " + " numero2 " + numero2 + " " + simNao);
+        if (condicao1 || condicao2) {
+            System.out.println("Uma das condições é verdadeira");
+        }
 
 
-        simNao = numero1 != numero2;
-        System.out.println("numero1 é diferente do numero2? " + "numero1 " + numero1 + " " + " numero2 " + numero2 + " " + simNao);
-
-        simNao = numero1 > numero2;
-        System.out.println("numero1 é maior que o numero2? " + "numero1 " + numero1 + " " + " numero2 " + numero2 + " " + simNao);
+        System.out.println("Fim");
     }
 }
