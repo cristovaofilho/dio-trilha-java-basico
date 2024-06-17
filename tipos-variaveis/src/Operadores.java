0f9add5342d3b723f8d18e9adc07aa96b4e3d66a
cristovao <academic.cristovao@gmail.com>
2024-06-17 11:48:49 -0400

    Operadores-5

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,13 @@
+public class Operadores {
+    public static void main(String[] args) {
+        // classe Operadores.java
+int a, b;
+
+a = 5;
+b = 6;
+
+String resultado = (a==b) ? "verdadeiro" : "false";
+
+System.out.println(resultado);
+    }
+}
